aaa7bf449be05d7a97ce6a6364b3a7299a3a2152
Anubhav Gajbhiye <114009237+anubhavfsd87@users.noreply.github.com>
2024-06-01 06:21:10 +0530

    Update ProfilePage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -40,7 +40,7 @@ export default function ProfilePage() {
                 {subpage === 'profile' && (
                     <div className="text-center max-w-lg mx-auto text-white text-1xl underline p-8">
                         Logged in as {user.name} ({user.email})<br />
-                        <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+                        <button onClick={logout} className="primary text-2xl max-w-sm mt-8">Logout</button>
                     </div>
                 )}
                 {subpage === 'books' && (
